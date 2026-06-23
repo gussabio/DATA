a714a170d5fdb79b3dd2bb71c9bbfd8e0b6ad20b
gussabio@gmail.com <gussabio@gmail.com>
2026-06-23 20:36:55 +0000

    Develop Branch

---
+++ models/mart/sales/dim_salesperson.sql
@@ -0,0 +1,11 @@
+{{
+    config(
+        tags=['dash_sales_bi']
+    )
+}}
+
+select
+    rank() over (order by salesperson) as sales_id,
+    salesperson
+from {{ ref('int_monthly_sales_make') }}
+group by salesperson
