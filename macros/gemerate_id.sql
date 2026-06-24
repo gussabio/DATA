4e8304c38439c38b5dc2932cff384bcc0bcf6f29
gussabio@gmail.com <gussabio@gmail.com>
2026-06-24 14:20:10 +0000

    Update test

---
+++ macros/gemerate_id.sql
@@ -0,0 +1,1 @@
+{%  macro generate_id(column_name) %} RANK() over (order by {{column_name}}) {% endmacro %}
--- models/mart/sales/dim_salesperson.sql
+++ models/mart/sales/dim_salesperson.sql
@@ -5,7 +5,8 @@
 }}
 
 select
-    rank() over (order by salesperson) as sales_id,
+    --rank() over (order by salesperson) as sales_id,
+    {generate_id('SALESPERSON')}} as sales_id,
     salesperson
 from {{ ref('int_monthly_sales_make') }}
 group by salesperson
